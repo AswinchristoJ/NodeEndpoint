4785a6ba09b5eaa7c68157035ffc35a98e0fc2f6
AswinChristo <aswinj2019@srishakthi.ac.in>
2019-07-22 19:43:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+
+app.get('/',(req,res)=>{
+console.log(req.query)
+res.send(`<form action="/get" method="get">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+</form>`)
+})
+
+app.get('/get',(req,res)=>{
+console.log(req.query)
+res.send(req.query)
+
+axios.post('https://myburger-b33d0.firebaseio.com/locaswin.json',{
+'firstName': 'Fred',
+    'lastName': 'Flintstone'
+})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+})
+
+})
+
+app.listen(4000,()=>{
+console.log('yup listening')
+})
